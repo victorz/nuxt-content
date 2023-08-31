677e39151819d8ad1d238717e4ae4c0219b5d21e
Victor Zamanian <victor.zamanian@gmail.com>
2023-08-31 09:58:11 +0200

    Add main site index file.

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <nav>
+      <menu>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/articles">Articles</NuxtLink>
+        </li>
+      </menu>
+    </nav>
+  </header>
+  <main>
+    <h1>This is the index</h1>
+  </main>
+</template>
