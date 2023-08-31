2193e9d1a6717c4bfb1d885ca1e67388db5186ec
Victor Zamanian <victor.zamanian@gmail.com>
2023-08-31 09:57:47 +0200

    Add global CSS file.

---
+++ pages/articles/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <NuxtLink to="/">Home</NuxtLink>
+    <h1>This is the articles index.</h1>
+    <ul>
+      <li v-for="post in posts" :key="post._id">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script setup lang="ts">
+const qc = queryContent("/articles");
+const { data } = useAsyncData("posts", () => qc.find());
+const posts = ref<typeof data.value>(data.value ?? []);
+</script>
